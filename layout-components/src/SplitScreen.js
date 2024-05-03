bb1429e1331c978d270ef623933d49bff9bc55b2
Steven <stevengf14@gmail.com>
2024-05-02 22:06:51 -0500

    split-screen component improvements

--- layout-components/src/SplitScreen.js
+++ layout-components/src/SplitScreen.js
@@ -5,18 +5,15 @@ const Container = styled.div`
 `;
 
 const Pane = styled.div`
-  flex: 1;
+  flex: ${(props) => props.weight};
 `;
 
-export const SplitScreen = ({ left: Left, right: Right }) => {
+export const SplitScreen = ({ children, leftWeight = 1, rightWeight = 1 }) => {
+  const [left, right] = children;
   return (
     <Container>
-      <Pane>
-        <Left />
-      </Pane>
-      <Pane>
-        <Right />
-      </Pane>
+      <Pane weight={leftWeight}>{left}</Pane>
+      <Pane weight={rightWeight}>{right}</Pane>
     </Container>
   );
 };
