6c1c7afb20bbae79ac5605e7867102fee09736f3
Steven <stevengf14@gmail.com>
2024-05-06 22:18:37 -0500

    UserLoader component

--- container-components/src/App.js
+++ container-components/src/App.js
@@ -1,11 +1,20 @@
-import { CurrentUserLoader } from "./CurrentUserLoader";
+//import { CurrentUserLoader } from "./CurrentUserLoader";
 import { UserInfo } from "./UserInfo";
+import { UserLoader } from "./UserLoader";
 
 function App() {
   return (
-    <CurrentUserLoader>
-      <UserInfo />
-    </CurrentUserLoader>
+    <>
+      <UserLoader userId="123">
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId="124">
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId="125">
+        <UserInfo />
+      </UserLoader>
+    </>
   );
 }
 
