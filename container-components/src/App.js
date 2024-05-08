b4bc64ef6a99f27fd20f223cf862285d8e82bd92
Steven <stevengf14@gmail.com>
2024-05-07 22:06:15 -0500

    DataSource component

--- container-components/src/App.js
+++ container-components/src/App.js
@@ -1,18 +1,22 @@
 //import { CurrentUserLoader } from "./CurrentUserLoader";
+import axios from "axios";
+import { DataSource } from "./DataSource";
 import { ProductInfo } from "./ProductInfo";
 import { ResourceLoader } from "./ResourceLoader";
 import { UserInfo } from "./UserInfo";
 import { UserLoader } from "./UserLoader";
 
+const getServerData = (url) => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
 function App() {
   return (
     <>
-      <ResourceLoader resourceUrl="/users/123" resourceName="user">
+      <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
         <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceUrl="/products/241" resourceName="product">
-        <ProductInfo />
-      </ResourceLoader>
+      </DataSource>
     </>
   );
 }
