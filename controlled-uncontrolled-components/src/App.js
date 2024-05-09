92b515959ebfd31691a7d4f037c02b86ee3f9f3e
Steven <stevengf14@gmail.com>
2024-05-08 22:21:34 -0500

    Uncontrolled onboarding flows

--- controlled-uncontrolled-components/src/App.js
+++ controlled-uncontrolled-components/src/App.js
@@ -1,23 +1,31 @@
-import { ControlledForm } from "./ControlledForm";
-import { UncontrolledModal } from "./UncontrolledModal";
-import { UncontrolledForm } from "./UncontrolledForm";
-import { ControlledModal } from "./ControlledModal";
-import { useState } from "react";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step 3</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
 
 function App() {
-  const [shouldShowModal, setShouldShowModal] = useState(false);
   return (
-    <>
-      <ControlledModal
-        shouldShow={shouldShowModal}
-        onRequestClose={() => setShouldShowModal(false)}
-      >
-        <h1>Hello!</h1>
-      </ControlledModal>
-      <button onClick={() => setShouldShowModal(!shouldShowModal)}>
-        {shouldShowModal ? "Hide Modal" : "Show Modal"}
-      </button>
-    </>
+    <UncontrolledOnboardingFlow>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
   );
 }
 
