7d0ea2063fea91fa7ed552b532b7d76f75473d1b
Steven <stevengf14@gmail.com>
2024-05-09 22:39:54 -0500

    printing props with HOCs

--- higher-order-components/src/App.js
+++ higher-order-components/src/App.js
@@ -1,5 +1,10 @@
+import { UserInfo } from "./UserInfo";
+import { printProps } from "./printProps";
+
+const UserInfoWrapped = printProps(UserInfo);
+
 function App() {
-  return <h1>Your code goes here</h1>;
+  return <UserInfoWrapped a={1} b="Hello" c={{ name: "Shaun" }} />;
 }
 
 export default App;
