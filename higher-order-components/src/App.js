475c5d9a0e5c5dc55f6ee02f8e8d3d32bd0b058c
Steven <stevengf14@gmail.com>
2024-05-09 23:17:05 -0500

    Creating forms with HOCs

--- higher-order-components/src/App.js
+++ higher-order-components/src/App.js
@@ -1,11 +1,12 @@
 import { withUser } from "./withUser";
 import { UserInfo } from "./UserInfo";
 import { printProps } from "./printProps";
+import { UserInfoForm } from "./UserInfoForm";
 
 const UserInfoWithLoader = withUser(UserInfo, "123");
 
 function App() {
-  return <UserInfoWithLoader />;
+  return <UserInfoForm />;
 }
 
 export default App;
